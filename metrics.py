5667eaf848281a81f48fc18f931b26379b6353cc
Drew Stone <drewstone329@gmail.com>
2017-11-14 18:43:48 -0500

    spacing

--- metrics.py
+++ metrics.py
@@ -2,36 +2,36 @@ import numpy as np
 from sklearn.metrics import matthews_corrcoef
 
 def matthews_correlation(model, X, Y):
-	"""
-	Computes matthews correlation coefficient amongst output labels
-	to find best threshold for future predictions
+    """
+    Computes matthews correlation coefficient amongst output labels
+    to find best threshold for future predictions
 
-	Returns predicted values on testset, class-level accuracies, and thesholds
-	"""
-	out = model.predict_proba(X)
-	out = np.array(out)
+    Returns predicted values on testset, class-level accuracies, and thesholds
+    """
+    out = model.predict_proba(X)
+    out = np.array(out)
 
-	threshold = np.arange(0.1,0.9,0.1)
+    threshold = np.arange(0.1,0.9,0.1)
 
-	acc = []
-	accuracies = []
-	best_threshold = np.zeros(out.shape[1])
-	for i in range(out.shape[1]):
-	    y_prob = np.array(out[:,i])
-	    for j in threshold:
-	        y_pred = [1 if prob>=j else 0 for prob in y_prob]
-	        acc.append( matthews_corrcoef(Y[:,i],y_pred))
-	    acc   = np.array(acc)
-	    index = np.where(acc==acc.min()) 
-	    accuracies.append(acc.max()) 
-	    best_threshold[i] = threshold[index[0][0]]
-	    acc = []
+    acc = []
+    accuracies = []
+    best_threshold = np.zeros(out.shape[1])
+    for i in range(out.shape[1]):
+        y_prob = np.array(out[:,i])
+        for j in threshold:
+            y_pred = [1 if prob>=j else 0 for prob in y_prob]
+            acc.append( matthews_corrcoef(Y[:,i],y_pred))
+        acc   = np.array(acc)
+        index = np.where(acc==acc.min()) 
+        accuracies.append(acc.max()) 
+        best_threshold[i] = threshold[index[0][0]]
+        acc = []
 
-	y_pred = np.array([[1 if out[i,j]>=best_threshold[j] else 0 for j in range(Y.shape[1])] for i in range(len(Y))])
-	return y_pred, accuracies, best_threshold
+    y_pred = np.array([[1 if out[i,j]>=best_threshold[j] else 0 for j in range(Y.shape[1])] for i in range(len(Y))])
+    return y_pred, accuracies, best_threshold
 
 def recall_accuracy(model, X, Y):
-	"""
-	Proportion of true labels vs. what we got
-	"""
-	return
+    """
+    Proportion of true labels vs. what we got
+    """
+    return
