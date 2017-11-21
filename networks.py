24f8d246ace35ed01d69b4c4fbe005472e2156a1
Drew Stone <drewstone329@gmail.com>
2017-11-21 13:51:51 -0500

    Adds more cnn + mlp network

--- networks.py
+++ networks.py
@@ -1,8 +1,7 @@
 import os
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Embedding
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, BatchNormalization
 from keras.layers import Conv1D, MaxPooling1D, GlobalAveragePooling1D, Dropout
 from keras.models import Model
 
@@ -28,6 +27,47 @@ def create_dnn():
     intermediate.add(Dense(256, activation='sigmoid', input_dim=47236, name='dense-input'))
     intermediate.add(Dense(256, activation='sigmoid', name='dense-layer2'))
     intermediate.add(Dense(256, activation='sigmoid', name='dense-layer3'))
+    intermediate.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    shallow = Sequential()
+    shallow.add(Dense(256, activation='sigmoid', input_dim=256))
+    shallow.add(Dense(103, activation='sigmoid'))
+    shallow.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model, intermediate, shallow
+
+
+def create_mlp():
+    """
+    Creates a basic deep neural network model
+    Input dimension matches a 47236 dimension feature vector
+    3 hidden layers of 256 neurons
+    """
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(47236, 1), name='batch-norm1'))
+    model.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    model.add(Dropout(rate=0.5, name='drop-layer1'))
+    model.add(BatchNormalization(name='batch-norm2'))
+    model.add(Dense(256, activation='relu', name='dense-layer2'))
+    model.add(Dropout(rate=0.5, name='drop-layer2'))
+    model.add(BatchNormalization(name='batch-norm3'))
+    model.add(Dense(256, activation='relu', name='dense-layer3'))
+    model.add(Dropout(rate=0.5, name='drop-layer3'))
+    model.add(Dense(256, activation='sigmoid'))
+    model.add(Dense(103, activation='sigmoid'))
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    intermediate = Sequential()
+    intermediate.add(BatchNormalization(input_shape=(47236, 1), name='batch-norm1'))
+    intermediate.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    intermediate.add(Dropout(rate=0.5, name='drop-layer1'))
+    intermediate.add(BatchNormalization(name='batch-norm2'))
+    intermediate.add(Dense(256, activation='relu', name='dense-layer2'))
+    intermediate.add(Dropout(rate=0.5, name='drop-layer2'))
+    intermediate.add(BatchNormalization(name='batch-norm3'))
+    intermediate.add(Dense(256, activation='relu', name='dense-layer3'))
+    intermediate.add(Dropout(rate=0.5, name='drop-layer3'))
+    intermediate.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
     shallow = Sequential()
     shallow.add(Dense(256, activation='sigmoid', input_dim=256))
@@ -38,8 +78,62 @@ def create_dnn():
 
 def create_cnn():
     """
-    Creates a neural network with an intial embedding layer
-    and a convolution layer
+    Creates a simple convolutional net
+    with a single conv layer
+    """
+    model = Sequential()
+    model.add(Conv1D(64, 3, padding='valid', activation='relu', strides=1, input_shape(47236, 1), name='conv-input'))
+    model.add(GlobalMaxPooling1D(name='gmp-layer'))
+    model.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    model.add(Dropout(0.2, name='drop-layer1'))
+    model.add(Dense(256, activation='sigmoid'))
+    model.add(Dense(103, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    intermediate = Sequential()
+    intermediate.add(Conv1D(64, 3, padding='valid', activation='relu', strides=1, input_shape(47236, 1), name='conv-input'))
+    intermediate.add(GlobalMaxPooling1D(name='gmp-layer'))
+    intermediate.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    intermediate.add(Dropout(0.2, name='drop-layer1'))
+    intermediate.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    shallow.add(Dense(256, activation='sigmoid'))
+    shallow.add(Dense(103, activation='sigmoid'))
+    shallow.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model, intermediate, shallow
+
+def create_deeper_cnn():
+    """
+    Creates a simple convolutional net
+    with a single conv layer
+    """
+    model = Sequential()
+    model.add(Conv1D(256, 3, padding='valid', activation='relu', strides=1, input_shape(47236, 1), name='conv-input'))
+    model.add(GlobalMaxPooling1D(name='gmp-layer'))
+    model.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    model.add(Dropout(0.2, name='drop-layer1'))
+    model.add(Dense(256, activation='sigmoid'))
+    model.add(Dense(103, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    intermediate = Sequential()
+    intermediate.add(Conv1D(256, 3, padding='valid', activation='relu', strides=1, input_shape(47236, 1), name='conv-input'))
+    intermediate.add(GlobalMaxPooling1D(name='gmp-layer'))
+    intermediate.add(Dense(256, activation='relu', name: 'dense-layer1'))
+    intermediate.add(Dropout(0.2, name='drop-layer1'))
+    intermediate.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    shallow.add(Dense(256, activation='sigmoid'))
+    shallow.add(Dense(103, activation='sigmoid'))
+    shallow.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model, intermediate, shallow
+
+def create_large_cnn():
+    """
+    Creates a complex convolutional neural network
+    with multiple convolutional filters
     """
     model = Sequential()
     model.add(Conv1D(64, 3, activation='relu', input_shape=(47236, 1), name='conv-input'))
@@ -49,6 +143,7 @@ def create_cnn():
     model.add(Conv1D(128, 3, activation='relu', name='conv-layer4'))
     model.add(GlobalAveragePooling1D(name='gp-layer'))
     model.add(Dropout(0.5, name='dropout'))
+    model.add(Dense(256, activation='sigmoid', name='dense-layer'))
     model.add(Dense(256, activation='sigmoid'))
     model.add(Dense(103, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -61,6 +156,9 @@ def create_cnn():
     intermediate.add(Conv1D(128, 3, activation='relu', name='conv-layer4'))
     intermediate.add(GlobalAveragePooling1D(name='gp-layer'))
     intermediate.add(Dropout(0.5, name='dropout'))
+    intermediate.add(Dense(256, activation='sigmoid', name='dense-layer'))
+    intermediate.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 
     shallow = Sequential()
     shallow.add(Dense(256, activation='sigmoid', input_dim=128))
--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ import re
 import json
 import random
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, find
 from sklearn.datasets import fetch_rcv1
 
 
@@ -22,35 +22,66 @@ def get_data(split_type='random'):
         return split(rcv1, split_type)
 
 def split(rcv1, split_type):
+    def reusable_split(split):
+        return rcv1.data[np.array(split)], rcv1.target[np.array(split)]
+
     data = json.load(open('categories.json'))
     rows = int(rcv1.data.shape[0])
     if split_type == 'random':
         first_split = random.sample(range(rows), int(rows/2))
         second_split = np.delete(np.arange(rows), first_split, 0)
+        
     elif split_type == 'simple':
         first_split = np.arange(int(rows/2))
         second_split = np.delete(np.arange(rows), first_split, 0)
+
     elif split_type == 'c_topics':
-        rgx = re.compile('C[0-9]')
+        rgx = re.compile('C[0-9]*')
         splitvec = np.zeros(len(data.keys()))
         for inx, elt in enumerate(data.keys()):
             if rgx.match(elt):
                 splitvec[inx] = 1
-        print(splitvec)
+
+        splitvec_ind = find(csr_matrix(splitvec))[1]
+        max_ind, min_ind = np.amax(splitvec_ind), np.amin(splitvec_ind)
 
 
-    return split_and_save(rcv1, split_type, first_split, second_split)
+        first_split, second_split, holdout_split = [], [], []
+        for inx, elt in enumerate(rcv1.target):
+            elt_ind = find(elt)[1]
+            elt_max_ind, elt_min_ind = np.amax(splitvec_ind), np.amin(splitvec_ind)
+
+            if elt_max_ind <= max_ind:
+                first_split.append(inx)
+            elif elt_min_ind > max_ind:
+                second_split.append(inx)
+            else:
+                holdout_split.append(inx)
+        
+
+    first, second = reusable_split(first_split), reusable_split(second_split)
+
+    if holdout_split:
+        holdout = reusable_split(holdout_split)
+        return save(split_type, first, second, holdout)
+
+    return save(split_type, first, second, None)
 
 
-def split_and_save(rcv1, split_type, first_split, second_split):
-    x1, y1 = rcv1.data[np.array(first_split)], rcv1.target[np.array(first_split)]
-    x2, y2 = rcv1.data[np.array(second_split)], rcv1.target[np.array(second_split)]
+def save(split_type, first, second, holdout):
+    x1, y1 = first
+    x2, y2 = second
 
     create_dirs(split_type)
     save_sparse_csr('data/{}/first_data'.format(split_type), x1)
     save_sparse_csr('data/{}/first_labels'.format(split_type), y1)
     save_sparse_csr('data/{}/second_data'.format(split_type), x2)
     save_sparse_csr('data/{}/second_labels'.format(split_type), y2)
+
+    if holdout:
+        x3, y3 = holdout
+        save_sparse_csr('data/{}/holdout_data'.format(split_type), x3)
+        save_sparse_csr('data/{}/holdout_labels'.format(split_type), y3)
 
     return x1, y1, x2, y2
 
